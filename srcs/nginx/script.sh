cd54a2eef81b9e1369500fa58df3e4012c70fda1
Ayoub Elhioui <Elhioui001@gmail.com>
2023-03-31 11:49:08 +0000

    a lot of issues with this configuration file

--- srcs/nginx/script.sh
+++ srcs/nginx/script.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-service mysql start & service php7.3-fpm start & nginx -g "daemon off;" \
-echo "CREATE DATABASE IF NOT EXISTS test ;" > db1.sql \
-echo "CREATE USER IF NOT EXISTS 'ayoub'@'%' IDENTIFIED BY '123456789' ;" >> db1.sql \
-echo "GRANT ALL PRIVILEGES ON test.* TO 'ayoub'@'%' ;" >> db1.sql \
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql \
-echo "FLUSH PRIVILEGES;" >> db1.sql \
+service mysql start
+service php7.3-fpm start
+nginx -g "daemon off;"
 
-sed -i -r "s/database_name_here/test/1"   /var/www/html/wp-config-sample.php \
-sed -i -r "s/username_here/ayoub/1"  /var/www/html/wp-config-sample.php \
-sed -i -r "s/password_here/123456789/1"    /var/www/html/wp-config-sample.php \
+# Create database and user
+echo "CREATE DATABASE IF NOT EXISTS test ;
+CREATE USER IF NOT EXISTS 'ayoub'@'%' IDENTIFIED BY '123456789' ;
+GRANT ALL PRIVILEGES ON test.* TO 'ayoub'@'%' ;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;
+FLUSH PRIVILEGES;" > db1.sql
+
 mysql < db1.sql
+
+# Copy wp-config-sample.php to wp-config.php
+chown -R www-data:www-data /var/www/html/
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+# Modify wp-config.php with database credentials
+sed -i "s/define('DB_NAME', 'database_name_here');/define('DB_NAME', 'test');/" /var/www/html/wp-config.php
+sed -i "s/define('DB_USER', 'username_here');/define('DB_USER', 'ayoub');/" /var/www/html/wp-config.php
+sed -i "s/define('DB_PASSWORD', 'password_here');/define('DB_PASSWORD', '123456789');/" /var/www/html/wp-config.php
+/bin/bash /tmp/script.php
+#sed -i "s/define('DB_USER', 'username_here');/define('DB_USER', 'test');/" /var/www/html/wp-config-sample.php
+# Update WordPress config file
+
+#sed -i -r "s/database_name_here/test/1" /var/www/html/wp-config-sample.php
+#sed -i -r "s/username_here/ayoub/1" /var/www/html/wp-config-sample.php
+#sed -i -r "s/password_here/123456789/1" /var/www/html/wp-config-sample.php
