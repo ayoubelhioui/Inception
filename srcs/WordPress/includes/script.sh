ebfc3b8d0f1b4ada636f06e06fa6155a5046fcd7
Ayoub Elhioui <Elhioui001@gmail.com>
2023-04-03 16:13:54 +0000

    moving forward

--- srcs/WordPress/includes/script.sh
+++ srcs/WordPress/includes/script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-mdkir -p /var/www/html
+mkdir -p /var/www/html
 cd /var/www/html
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
-wp core download
+wp core download --allow-root
+
+tail -f
+#/usr/sbin/php-fpm7.3 -F -R
 
