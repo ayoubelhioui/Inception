35836b011418cacd2e5fe0a2777ecfe166fd82c2
ayoubelhioui <Elhioui001@gmail.com>
2023-05-01 17:55:08 +0100

    almost finishing this project

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ clean:
 	docker rm $$(docker ps -qa);\
 	docker rmi -f $$(docker images -qa);\
 	docker volume rm $$(docker volume ls -q);\
- 	sudo rm -rf /home/ael-hiou/data/mariadb\
+	docker network rm $(docker network ls -q);\
+ 	sudo rm -rf /home/ael-hiou/data/mysql;\
 	sudo rm -rf /home/ael-hiou/data/wordpress
-.PHONY: all re down clean
+.PHONY: all down clean
--- srcs/requirements/WordPress/tools/script.sh
+++ srcs/requirements/WordPress/tools/script.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 mkdir -p /var/www/html
 mkdir -p /run/php
-chown www-data:www-data /run/php
 cd /var/www/html
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/xx
 wp core download --allow-root
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sed -i "s/username_here/$USERNAME/g" wp-config.php
 sed -i "s/password_here/$PASSWORD/g" wp-config.php
 sed -i "s/localhost/$HOST/g" wp-config.php
 sed -i "s/database_name_here/$DATABASE_NAME/g" wp-config.php
-# sed -i "s/listen = /run/php/php7.3-fpm.sock/listen = 9000" /etc/php/7.3/fpm/pool.d/www.conf
 sed -i 's/^listen = \/run\/php\/php7\.3-fpm\.sock$/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 wp core install --url="localhost" --title="Example" --admin_user=$WP_ADMIN_NAME --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --allow-root
 wp user create $WP_USER_NAME $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
 php-fpm7.3 -F
-tail -f
